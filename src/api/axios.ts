af780aa5f3168aa53989a719b35ce87a751f80a3
mirage0720 <youdw1994@gmail.com>
2025-09-10 12:54:41 +0900

    initial commit

---
+++ src/api/axios.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export const axioisInstance = axios.create({
+  baseURL: "http://localhost:4000",
+  withCredentials: true,
+});
+
+axioisInstance.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem("access_token");
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+let retry = false;
+axioisInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response && error.response.status === 401 && !retry) {
+      retry = true;
+      try {
+        const res = await axioisInstance.post("/auth/refresh");
+        if (!res.data.accessToken) throw new Error("Access token is missing");
+        retry = false;
+        sessionStorage.setItem("access_token", res.data.accessToken);
+        return axioisInstance(originalRequest);
+      } catch (refreshError) {
+        sessionStorage.removeItem("access_token");
+        await axioisInstance.post("/auth/logout");
+        return Promise.reject(refreshError);
+      }
+    }
+  }
+);
---
+++ src/routes/loader/auth.loader.ts
@@ -0,0 +1,31 @@
+import { redirect } from "react-router";
+import { axioisInstance } from "../../api/axios";
+import { useAuthStore } from "../../store/authStore";
+
+export const fetchuserData = async () => {
+  try {
+    const user = useAuthStore.getState().user;
+    const accessToken = sessionStorage.getItem("access_token");
+    if (!user && accessToken) {
+      const { data } = await axioisInstance.get("/auth/me");
+      const setUserData = useAuthStore.getState().setUserData;
+      setUserData(data);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const requireAuth = () => {
+  const token = sessionStorage.getItem("access_token");
+  if (!token) {
+    return redirect("/auth/login");
+  }
+};
+
+export const redirectIfAuth = () => {
+  const token = sessionStorage.getItem("access_token");
+  if (token) {
+    return redirect("/");
+  }
+};
